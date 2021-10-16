6e1d041201665383c397732969f2138263b9c63c
gracecj <johnsongc3@vcu.edu>
2021-10-15 20:34:24 -0400

    added journal page and buttons

---
+++ app/src/main/java/com/example/app355/FreewriteActivity.java
@@ -0,0 +1,14 @@
+package com.example.app355;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreewriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_freewrite);
+    }
+}
