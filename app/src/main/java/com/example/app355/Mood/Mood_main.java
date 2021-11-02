0b37ab9dba7ea6fd97d7762a2e7a861932cab288
DaniyarTheGreat <daniyarsaifuding@gmail.com>
2021-11-02 15:55:06 -0400

    The template of first question page

--- app/src/main/java/com/example/app355/Mood/Mood_main.java
+++ app/src/main/java/com/example/app355/Mood/Mood_main.java
@@ -2,15 +2,32 @@ package com.example.app355.Mood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.app355.R;
 
 public class Mood_main extends AppCompatActivity {
+    private Button mood;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_main);
+
+        mood = findViewById(R.id.button2);
+        mood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMoodPage();
+            }
+        });
+    }
+    public void openMoodPage() {
+        Intent intent = new Intent(this, Mood_Q1.class);
+        startActivity(intent);
     }
 }
