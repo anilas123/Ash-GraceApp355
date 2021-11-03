e97710af5f9eda0baebbe2058aff08bbe4bffc6b
anilas123 <anilas@vcu.edu>
2021-11-03 16:05:41 -0400

    Calendar cell xml, CalendarAdapt and CalendarViewHolder java classes

---
+++ app/src/main/java/com/example/app355/CalendarAdapt.java
@@ -0,0 +1,24 @@
+package com.example.app355;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class CalendarAdapt extends RecyclerView.Adapter<CalendarViewHolder> {
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
