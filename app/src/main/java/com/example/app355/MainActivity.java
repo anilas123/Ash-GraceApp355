9e02d80d8c3e3251df6304a60cd3d70661a15a15
anilas123 <anilas@vcu.edu>
2021-11-03 17:37:28 -0400

    Calendar in Main Activity

--- app/src/main/java/com/example/app355/MainActivity.java
+++ app/src/main/java/com/example/app355/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app355;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -10,13 +11,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app355.Mood.Mood_main;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CalendarAdapt.OnItemLister {
 
     private Button journal;
     private Button calendar;
@@ -65,6 +69,45 @@ public class MainActivity extends AppCompatActivity {
 
     private void setMonthView() {
         monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapt calendarAdapt = new CalendarAdapt(daysInMonth, this);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapt);
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date) {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for(int i = 1; i <= 42; i++) {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
+                daysInMonthArray.add("");
+            }
+            else{
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+
+        return daysInMonthArray;
+
+
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if(dayText.equals("")) {
+            String message = "Selected Date" + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
+
     }
 
     private String monthYearFromDate(LocalDate date) {
@@ -79,9 +122,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void previousMonthAction(View view) {
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
     }
 
     public void nextMonthAction(View view) {
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
     }
     //Calendar code ends
 
@@ -100,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Mood_main.class);
         startActivity(intent);
     }
+
 
 
 }
