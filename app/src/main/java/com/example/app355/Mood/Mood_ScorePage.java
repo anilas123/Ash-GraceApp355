9f573c4dfbcf21d2999d8d2d1d38d71db4ca42e5
DaniyarTheGreat <daniyarsaifuding@gmail.com>
2021-11-05 22:43:33 -0400

    A little change

--- app/src/main/java/com/example/app355/Mood/Mood_ScorePage.java
+++ app/src/main/java/com/example/app355/Mood/Mood_ScorePage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.app355.R;
@@ -14,6 +15,7 @@ public class Mood_ScorePage extends AppCompatActivity {
     int i = 0;
 
     private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +23,32 @@ public class Mood_ScorePage extends AppCompatActivity {
         setContentView(R.layout.activity_mood_score_page);
 
         textView = findViewById(R.id.textView9);
+        button = findViewById(R.id.button);
 
-        i = moodQ1.getPoints();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Your mood is Happy");
+            }
+        });
 
-        if (i > 4 && i <= 5){
-            textView.setText("Your mood is happy");
-        }
-        else if (i >3 && i <= 4){
-            textView.setText("Your mood is calm");
-        }
-        else if (i >2 && i <= 3){
-            textView.setText("Your mood is annoyed");
-        }
-        else if (i >1 && i <= 2){
-            textView.setText("Your mood is upset");
-        }
-        else if (i >0 && i <= 1){
-            textView.setText("Your mood is sad");
-        }
+//        i = moodQ1.getPoints();
+//
+//        if (i > 4 && i <= 5){
+//            textView.setText("Your mood is happy");
+//        }
+//        else if (i >3 && i <= 4){
+//            textView.setText("Your mood is calm");
+//        }
+//        else if (i >2 && i <= 3){
+//            textView.setText("Your mood is annoyed");
+//        }
+//        else if (i >1 && i <= 2){
+//            textView.setText("Your mood is upset");
+//        }
+//        else if (i >0 && i <= 1){
+//            textView.setText("Your mood is sad");
+//        }
 
 
 
