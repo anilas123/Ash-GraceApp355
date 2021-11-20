0442db03fe2949e3eb8761937d574506e017ff0e
anilas123 <anilas@vcu.edu>
2021-11-20 12:35:59 -0500

    finished notes main page design

--- app/src/main/java/com/example/app355/FreewriteActivity.java
+++ app/src/main/java/com/example/app355/FreewriteActivity.java
@@ -1,9 +1,11 @@
 package com.example.app355;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class FreewriteActivity extends AppCompatActivity {
 
@@ -17,6 +19,24 @@ public class FreewriteActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.options_menu,menu);
+
+        MenuItem searchItem=menu.findItem(R.id.searchbar);
+        SearchView searchView= (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Search Notes Here");
+
+        SearchView.OnQueryTextListener listener=new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        };
+
+        searchView.setOnQueryTextListener(listener);
 
         return super.onCreateOptionsMenu(menu);
     }
