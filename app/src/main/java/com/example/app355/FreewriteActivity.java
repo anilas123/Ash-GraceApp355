d2f497a5ede004a59a5097de168652605095f2a3
anilas123 <anilas@vcu.edu>
2021-11-20 12:26:43 -0500

    created an options menu for notes page

--- app/src/main/java/com/example/app355/FreewriteActivity.java
+++ app/src/main/java/com/example/app355/FreewriteActivity.java
@@ -3,6 +3,7 @@ package com.example.app355;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class FreewriteActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class FreewriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_freewrite);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
