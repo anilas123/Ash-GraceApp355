753cd792b10009ec49ef47934ecaf74c39d2dd43
DaniyarTheGreat <daniyarsaifuding@gmail.com>
2021-11-20 22:18:33 -0500

    Fixing bugs

--- app/src/main/java/com/example/app355/PromptsActivity.java
+++ app/src/main/java/com/example/app355/PromptsActivity.java
@@ -3,12 +3,15 @@ package com.example.app355;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PromptsActivity extends AppCompatActivity {
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prompts);
+        textView.findViewById(R.id.textView);
     }
 }
