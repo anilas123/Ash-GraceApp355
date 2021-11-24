f9ba6edc8c7777ac2e759d1c4b7845a2e1ab096d
gracecj <johnsongc3@vcu.edu>
2021-11-24 02:11:17 -0500

    added another journal prompt

--- app/src/main/java/com/example/app355/PromptsActivity.java
+++ app/src/main/java/com/example/app355/PromptsActivity.java
@@ -27,8 +27,8 @@ public class PromptsActivity extends AppCompatActivity {
         promptList.add("Get in touch with your surroundings! Find a spot to sit for five minutes, write what you see, hear, and/or feel.");
         promptList.add("Write a list of short term goals for yourself. Make them realistic.");
         promptList.add("Write an apology to yourself. Forgive yourself for being human.");
-        promptList.add("Where do you want to be in 10 years? How will you achieve your dreams?");
-        promptList.add("prompt 7");
+        promptList.add("Where do you want to be in 10 years? How will you get there?");
+        promptList.add("List some of your favorite things in life. How do these things make you feel?");
         promptList.add("prompt 8");
 
         ArrayAdapter promptAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, promptList);
