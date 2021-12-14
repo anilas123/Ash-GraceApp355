aa809aea65e61640d361244b4542cf8e486ff248
gracecj <johnsongc3@vcu.edu>
2021-12-14 01:25:52 -0500

    Update AddNotesActivity.java

--- app/src/main/java/com/example/app355/AddNotesActivity.java
+++ app/src/main/java/com/example/app355/AddNotesActivity.java
@@ -3,6 +3,7 @@ package com.example.app355;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -27,7 +28,8 @@ public class AddNotesActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 if (!TextUtils.isEmpty(title.getText().toString()) && !TextUtils.isEmpty(description.getText().toString())) {
-
+                    DatabaseClass db = new DatabaseClass(AddNotesActivity.this);
+                    db.addNotes(title.getText().toString(),description.getText().toString());
                 }
                 else {
                     Toast.makeText(AddNotesActivity.this, "Both Fields Required", Toast.LENGTH_SHORT).show();
