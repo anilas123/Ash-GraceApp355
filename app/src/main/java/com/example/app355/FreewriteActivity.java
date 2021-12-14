3cfb02785a74d5d288ff3d3c624b2b954503824d
gracecj <johnsongc3@vcu.edu>
2021-12-14 11:39:55 -0500

    Update FreewriteActivity.java

--- app/src/main/java/com/example/app355/FreewriteActivity.java
+++ app/src/main/java/com/example/app355/FreewriteActivity.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.view.Display;
@@ -19,6 +20,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -127,6 +130,31 @@ public class FreewriteActivity extends AppCompatActivity {
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
             int position = viewHolder.getAdapterPosition();
+            NotesModel item = adapter.getList().get(position);
+
+            adapter.removeItem(viewHolder.getAdapterPosition());
+
+            Snackbar snackbar = Snackbar.make(coordinatorLayout, "item Deleted", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            adapter.restoreItem(item,position);
+                            recyclerView.scrollToPosition(position);
+                        }
+                    }).addCallback(new BaseTransientBottomBar.BaseCallback<Snackbar>() {
+                        @Override
+                        public void onDismissed(Snackbar transientBottomBar, int event) {
+                            super.onDismissed(transientBottomBar, event);
+
+                            if (!(event==DISMISS_EVENT_ACTION)) {
+                                DatabaseClass db = new DatabaseClass(FreewriteActivity.this);
+                                db.deleteSingleItem(item.getId());
+                            }
+                        }
+                    });
+
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.show();
         }
     };
 
